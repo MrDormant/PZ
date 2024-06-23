e087b1030027e23c481b4520ae72703cf8c8f3af
mrdormant <dremlyuga.vova.v2@mail.ru>
2024-06-23 12:43:52 +0300

    +- ladt comm

---
+++ PZ_17/17_3.py
@@ -0,0 +1,28 @@
+import os
+
+# task 1
+# os.chdir('../PZ_11')
+# print(*filter(lambda x: not os.path.isdir(x), os.listdir()))
+
+# task 2
+os.chdir('../..')
+if not os.path.exists('test'):
+    os.makedirs('test/test1')
+os.replace('tyjtj.txt', 'test/1.txt')
+os.replace('кцуАцу.txt', 'test/2.txt')
+os.replace('регу.txt', 'test/test1/1.txt')
+os.rename('test/test1/1.txt', 'test/test1/test.txt')
+print(f"Размер файла test/1.txt {os.path.getsize('test/1.txt')}")
+print(f"Размер файла test/2txt {os.path.getsize('test/2.txt')}")
+#
+# # task 3
+# os.chdir('PZ_11')
+# print(min([os.path.basename(i) for i in os.listdir()], key=len))
+#
+# # task 4
+# os.chdir('../reports')
+# os.startfile('PZ_2.pdf')
+#
+# # task 5
+# os.chdir('..')
+# os.remove('test/test1/test.txt')
